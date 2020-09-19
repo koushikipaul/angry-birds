4838ba62d5dbe91e75085072035b875678d994a9
koushikipaul <70462037+koushikipaul@users.noreply.github.com>
2020-09-19 10:48:59 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,18 @@
+class Bird extends Baseclass {
+    constructor(x, y) {
+      var options = {
+        'density':1.5,
+        'friction': 1.0,
+        'restitution':0.5
+      }
+      super(x,y,50,50,options);
+      this.image=loadImage("sprites/bird.png");
+    }
+    display(){
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      super.display();
+    }
+  }
+  
